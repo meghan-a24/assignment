aec256cca5434e418470b171790527829b4fc0c0
meghan-a24 <meghanapuram58@gmail.com>
2026-03-28 19:50:59 +0530

    Create meghana.java

---
+++ meghana.java
@@ -0,0 +1,10 @@
+public class meghana {
+    public static void main("String[] args"){
+        system.out.println("hello");
+        system.out.println("my name  is meghana");
+        system.out.println("iam from nzb");
+        system.out.pintln("iam pursuing btech 3rd yr");
+        system.out.println("In kits clg");
+        system.out.println("thank you");
+    }
+}
