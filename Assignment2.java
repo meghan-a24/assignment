60390ab90dfff6401dcb3aa54e2f8f90d29ddcf3
meghan-a24 <meghanapuram58@gmail.com>
2026-03-30 22:14:01 +0530

    commit3

--- Assignment2.java
+++ Assignment2.java
@@ -1,13 +1,21 @@
 public class studentinfo{
     public static void main(String[] args){
     String name="meghana";
+    String village="jankampet";
     int age=19;
+    String branch="cse";
+    int year=3;
     double percentage=7.55;
-    boolean ishosteler=ture;
+    boolean ishosteler=true;
     char section='B';
+    long phnnumber=9391259030L;
     System.out.println("name:"+name);
+    System.out.println("vlg:"+village);
     System.out.println("age:"+age);
     System.out.println("percentage:"+percentage+"%");
+    System.out.println("branch:"+branch);
+    System.out.println("year:"+year);
     System.out.println("Ishstl:"+ishosteler);
     System.out.println("sec:"+section);
+    System.out.println("phn no.:"+phnnumber);
     }}
