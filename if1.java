6d462a2d357bbdd2be40ec16fbc03f393d613dfc
meghan-a24 <meghanapuram58@gmail.com>
2026-04-09 22:30:04 +0530

    if2.java

---
+++ if1.java
@@ -0,0 +1,18 @@
+public class if1{
+    public static void main(String[] args){
+  boolean meghana=true;
+boolean keerthi=false;
+if (meghana){
+    System.out.println("Meghana is in 3rd year");
+    System.out.println("Meghana is not in 1styear");
+    System.out.println("Meghana is not in 2nd year");
+    System.out.println("Meghana is not in 4th year");
+}
+if(keerthi)
+    System.out.println("Keerthi is in 3rd year");
+    System.out.println("Keerthi is not in 1styear");
+    System.out.println("Keerthi is not in 2nd year");
+    System.out.println("Keerthi is not in 4th year");
+}
+
+}
