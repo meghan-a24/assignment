e7fd5ae0fb5403f4ce3532ac6c7aaebae47cec7a
meghan-a24 <meghanapuram58@gmail.com>
2026-04-26 22:27:29 +0530

    findlength

--- findlength.java
+++ findlength.java
@@ -1,5 +1,5 @@
 public class findlength{
-    public static void main(String[] agrs){}
+    public static void main(String[] agrs){
     string name="meghana from jankampet";
 
     System.out.println(name.length());
