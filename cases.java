91df049d8358572b2c89c66062a7145a7a5b4bf0
meghan-a24 <meghanapuram58@gmail.com>
2026-04-26 22:39:44 +0530

    cases1.java

---
+++ cases.java
@@ -0,0 +1,13 @@
+public class cases{
+    public static void main(String[] args)
+    {
+        String name1="meghana";
+        String name2="keerthana";
+        System.out.println(name1.toUpperCase());
+        System.out.println(name2.toLowerCase());
+        System.out.println(name2.toUpperCase());
+        System.out.println(name1.toLowerCase());
+
+
+    }
+}
