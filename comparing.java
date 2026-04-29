88d72d365474395d83a6d32c1781ea5e5b8fa6f7
meghan-a24 <meghanapuram58@gmail.com>
2026-04-29 13:46:58 +0530

    camparingStrings.java

---
+++ comparing.java
@@ -0,0 +1,13 @@
+public class comparingstring{
+    public static void main(String[]args)
+    {
+        String name="meghana";
+        String name1="keerthana";
+        String name2="meghana";
+        String name3="keerthana";
+        System.out.println(name.equals(name1));
+        System.out.println(name.equals(name2));
+        System.out.println(name1.equals(name3));
+
+    }
+}
