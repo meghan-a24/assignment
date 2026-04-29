abddec9c723ac6b21ad1384cb1320df61432f7ce
meghan-a24 <meghanapuram58@gmail.com>
2026-04-29 15:01:27 +0530

    escapechar.java

---
+++ escapeChar.java
@@ -0,0 +1,12 @@
+public class escapechar{
+    public static void main(String[]args)
+    {
+        System.out.println("puram\n meghana");//(\n next line)
+        System.out.println("puram\tmeghana"); //(\t  long space)
+System.out.println("puram\"meghana\"");
+System.out.println("puram \'meghana\'");
+System.out.println("puram\\meghana\\");
+System.out.println("puram\b");//(\b backspace)
+
+    }
+}
