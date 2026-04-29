adf994a7ebdcecf232b8de28c057b8c987d18e5a
meghan-a24 <meghanapuram58@gmail.com>
2026-04-29 14:09:46 +0530

    stringconcatenation.java

---
+++ concatenation.java
@@ -0,0 +1,13 @@
+public class  concatenation{
+    public static void main(String[]args)
+    {
+        String name="puram";
+        String name1="meghana";
+        String name2="yadav";
+        
+        System.out.println(name+ " "+name1);
+        System.out.println(name1.concat(name2));
+        
+
+    }
+}
