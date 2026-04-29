ada3b17fafffb27c0879c1509bc5710543283646
meghan-a24 <meghanapuram58@gmail.com>
2026-04-29 13:56:00 +0530

    eliminatingSpaces.java

---
+++ eliminate.java
@@ -0,0 +1,12 @@
+public class eliminatingSpaces{
+    public static void main(String[]args)
+    {
+        String name="meghana   ";
+        String name1="   keerthana   ";
+        
+        System.out.println(name1.trim());
+        System.out.println(name.trim());
+        
+
+    }
+}
