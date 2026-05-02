743446cc3052a99d58b80da58a7658b3481bf4d5
meghan-a24 <meghanapuram58@gmail.com>
2026-05-02 19:41:16 +0530

    int array.java

---
+++ array.java
@@ -0,0 +1,19 @@
+public class array{
+    int[]array;
+    public static void main(String[]args){ 
+       int[] array= new int[5];
+        array[0]=20;
+        array[1]=38;
+        array[2]=50;
+        array[3]=68;
+        array[4]=80;
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+        System.out.println(array[3]);
+        System.out.println(array[4]);
+
+
+    }
+    
+}
