aecaed9fcba376d36fefa2be805338ad1ea0e4a2
meghan-a24 <meghanapuram58@gmail.com>
2026-05-02 19:52:51 +0530

    integers as input.java

---
+++ array1.java
@@ -0,0 +1,11 @@
+public class array{
+    int[] number;
+public static void main(String[]args){
+    int[] number={24,33,43,34,56};
+      System.out.println(number[0]);
+      System.out.println(number[1]);
+      System.out.println(number[2]);
+      System.out.println(number[3]);
+      System.out.println(number[4]);
+}
+}
