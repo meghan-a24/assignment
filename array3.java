ea7eb70fe4ccd2e6856d9b03c1e23305ebf0d439
meghan-a24 <meghanapuram58@gmail.com>
2026-05-02 20:15:51 +0530

    even.java

---
+++ array3.java
@@ -0,0 +1,11 @@
+publicblic class array{
+    public static void main(String[]args){
+        int[]number={20,40,57,80,60};
+        for(int i=0;i<number.length;i++){
+         if(number[i]%2==0){
+       System.out.println(number[i]);
+}
+        }
+        
+    }
+}
